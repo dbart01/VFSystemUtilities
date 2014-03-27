fde04c8008d9d0cef435dd8480df158815cbf444
Dima Bart <dbart@Dimas-Mac-Mini.local>
2014-03-27 19:28:10 -0400

    - added a header to include all modules - some build setting changes

---
+++ VFSystemUtilities/VFSystemUtilities.h
@@ -0,0 +1,16 @@
+//
+//  VFSystemUtilities.h
+//
+//  Created by Dima Bart on 2014-03-27.
+//  Copyright (c) 2014 Dima Bart. All rights reserved.
+//
+
+#ifndef __VFSystemUtilities__
+#define __VFSystemUtilities__
+
+#import "VFFileManager.h"
+#import "VFMachine.h"
+#import "VFByteFormatter.h"
+#import "VFTokenCollection.h"
+
+#endif
